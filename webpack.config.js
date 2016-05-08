45b924f4feaf107a9cc5d12288cb43c2105367e8
sangamkaushik <sangam.k70@gmail.com>
2016-05-07 17:22:59 -0700

    Installed React

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/reactapp.jsx",
+  output: {
+    path: "./js",
+    publicPath: "/js/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  }
+};
